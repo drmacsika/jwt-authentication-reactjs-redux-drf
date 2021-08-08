d22205c71d5b1a4d226f3e269d6b64a75ff43e2a
Archangel Macsika <drmacsika@gmail.com>
2021-08-08 21:45:28 +0100

    Added the base file for the Frontend in ReactJS

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// import Home from "./containers/Home";
+// import Login from "./containers/Login";
+// import Signup from "./containers/Signup";
+// import Activate from "./containers/Activate";
+// import ResetPassword from "./containers/ResetPassword";
+// import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
+// import Facebook from "./containers/Facebook";
+// import Google from "./containers/Google";
+
+import { Provider } from "react-redux";
+// import store from './store';
+
+import Layout from "./layouts/Layout";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/layouts/Layout.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+// import Navbar from '../components/Navbar';
+// import { connect } from 'react-redux';
+// import { checkAuthenticated, load_user } from '../actions/auth';
+// { checkAuthenticated, load_user, children }
+const Layout = (props) => {
+  // useEffect(() => {
+  //     checkAuthenticated();
+  //     load_user();
+  // }, []);
+
+  return (
+    <div>
+      {/* <Navbar /> */}
+      {/* {children} */}
+    </div>
+  );
+};
+
+export default Layout;
+// connect(null, { checkAuthenticated, load_user })(Layout);
